eb73c3f28ffaecfe93bf5c778cb03ed26645d5d1
Eldho K K <101356753+Eldho1416@users.noreply.github.com>
2023-01-06 20:34:17 +0530

    Update Fw_mqtt_class.java

--- fw_IIoT_mqtt_pub/src/main/java/fw_IIoT/mqtt_sample_single_run/Fw_mqtt_class.java
+++ fw_IIoT_mqtt_pub/src/main/java/fw_IIoT/mqtt_sample_single_run/Fw_mqtt_class.java
@@ -27,105 +27,106 @@ public class Fw_mqtt_class {
 	int qos = 0;
 	
 	public Fw_mqtt_class() throws MqttException {
+		
+	    try {
+		// Construct the connection options object that contains connection parameters
+		conOpt = new MqttConnectOptions();
+		conOpt.setCleanSession(true);
+		conOpt.setUserName(username);
+		conOpt.setPassword(password.toCharArray());
 
-	    	try {
-	    		// Construct the connection options object that contains connection parameters
-		    	conOpt = new MqttConnectOptions();
-		    	conOpt.setCleanSession(true);
-		    	conOpt.setUserName(username);
-		    	conOpt.setPassword(password.toCharArray());
-		    	
-	    		// Construct an MQTT blocking mode client
-		    	if(port == 1883){
-		    		client = new MqttClient("tcp://"+brokerUrl+":"+Integer.toString(port), clientId, new MemoryPersistence());
-		    	}
-		    	else if(port == 8883) {
-		    		conOpt.setSocketFactory(SSLSocketFactory.getDefault());
-		    		client = new MqttClient("ssl://"+brokerUrl+":"+Integer.toString(port), clientId, new MemoryPersistence());
-		    	}
+		// Construct an MQTT blocking mode client
+		if(port == 1883){
+			client = new MqttClient("tcp://"+brokerUrl+":"+Integer.toString(port), clientId, new MemoryPersistence());
+		}
+		else if(port == 8883) {
+			conOpt.setSocketFactory(SSLSocketFactory.getDefault());
+			client = new MqttClient("ssl://"+brokerUrl+":"+Integer.toString(port), clientId, new MemoryPersistence());
+		}
+		else{
+			throw new IllegalArgumentException("Please enter a vaild port.");
+		}
+
 				
+		// Set this wrapper as the callback handler
+		client.setCallback(new MqttCallback() {
 
-				
-				// Set this wrapper as the callback handler
-		    	client.setCallback(new MqttCallback() {
-					
-		    		// Check if any message is received  
-					@Override
-					public void messageArrived(String sub_topic, MqttMessage message) throws Exception {
-						System.out.println("---Message received from " + sub_topic + "---\n" +
-								"Message: " + new String(message.getPayload()) +
-								" QoS: " + message.getQos() +"\n" +
-								"----------------------------------------------------------------\n");
-					}
-					
-					// Check if data is sent successfully 
-					@Override
-					public void deliveryComplete(IMqttDeliveryToken token) {
+			// Check if any message is received  
+			@Override
+			public void messageArrived(String sub_topic, MqttMessage message) throws Exception {
+				System.out.println("---Message received from " + sub_topic + "---\n" +
+						"Message: " + new String(message.getPayload()) +
+						" QoS: " + message.getQos() +"\n" +
+						"----------------------------------------------------------------\n");
+			}
 
-			        	try {
-							System.out.println("Succesfully published!\n"  +"Payload: "+token.getMessage() + "\n");
-						} catch (MqttException e) {
-							e.printStackTrace();
-						}
-					}
-					
-					// Reconnect if connection is lost
-					@Override 
-					public void connectionLost(Throwable cause) {
-
-						System.out.println("Connection to " + brokerUrl + " lost!" + cause.getMessage());
-						System.out.println("Reconnecting..");
-			        	try {
-							client.connect(conOpt);
-				        	System.out.println("Connected to Fogwing IIoT cloud");
-						} catch (MqttException e) {
-							e.printStackTrace();
-						}
-					}
-				});
-		    	if(client.isConnected()){
-		        	System.out.println("was already Connected");
-		        	client.disconnect();
-		    	}
-	        	client.connect(conOpt);
-	        	System.out.println("Connected to Fogwing IIoT cloud");
-
-
+			// Check if data is sent successfully 
+			@Override
+			public void deliveryComplete(IMqttDeliveryToken token) {
+			try {
+				System.out.println("Succesfully published!\n"  +"Payload: "+token.getMessage() + "\n");
 			} catch (MqttException e) {
 				e.printStackTrace();
-				System.out.println("Unable to set up client: "+e.toString());
-				System.exit(1);
 			}
-	    }
-		
-		public void subscribe() throws MqttPersistenceException, MqttException {
-			if(client.isConnected()) {
-				client.subscribe(sub_topic, qos);
-				System.out.println("Subscribed to "+ sub_topic+"\n");
 			}
-			else {
+
+			// Reconnect if connection is lost
+			@Override 
+			public void connectionLost(Throwable cause) {
+
+				System.out.println("Connection to " + brokerUrl + " lost!" + cause.getMessage());
+				System.out.println("Reconnecting..");
+			try {
 				client.connect(conOpt);
 				System.out.println("Connected to Fogwing IIoT cloud");
-				System.out.println("Subscribed to "+ sub_topic+"\n");
-			}
+			} catch (MqttException e) {
+				e.printStackTrace();
+			}}
+		});
+		if(client.isConnected()){
+			System.out.println("was already Connected");
+			client.disconnect();
 		}
-			
-		
-	    public void publish(String data) throws MqttPersistenceException, MqttException, InterruptedException{
-	    	System.out.println("Publishing data to " +pub_topic);
-	    	// Create and configure a message
-	   		MqttMessage message = new MqttMessage(data.getBytes());
-	    	message.setQos(qos);
-	    	message.setRetained(false);
-	    	client.publish(pub_topic, message);
-	    	
+		// Connect to Fogwing MQTT
+		client.connect(conOpt);
+		System.out.println("Connected to Fogwing IIoT cloud");
 
-	    }
-	    
-	    // This Function is to close MQTT connection
-	    public void close() throws MqttException{
-	    	// Disconnect the client
-	    	client.disconnect();
-	    	System.out.println("Disconnected");
-	    }
+
+		} catch (MqttException e) {
+			e.printStackTrace();
+			System.out.println("Unable to set up client: "+e.toString());
+			System.exit(1);
+		}
+	}
+
+	
+	// This Function is to subscribe to topic
+	public void subscribe() throws MqttPersistenceException, MqttException {
+		if(client.isConnected()) {
+			client.subscribe(sub_topic, qos);
+			System.out.println("Subscribed to "+ sub_topic+"\n");
+		}
+		else {
+			client.connect(conOpt);
+			System.out.println("Connected to Fogwing IIoT cloud");
+			System.out.println("Subscribed to "+ sub_topic+"\n");
+		}
+	}
+
+	// This Function is to publish data to topic
+	public void publish(String data) throws MqttPersistenceException, MqttException, InterruptedException{
+	System.out.println("Publishing data to " +pub_topic);
+	// Create and configure a message
+	MqttMessage message = new MqttMessage(data.getBytes());
+	message.setQos(qos);
+	message.setRetained(false);
+	client.publish(pub_topic, message);
+	}
+
+	// This Function is to close MQTT connection
+	public void close() throws MqttException{
+	// Disconnect the client
+	client.disconnect();
+	System.out.println("Disconnected");
+	}
 }
